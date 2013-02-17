be2d50e27ae210a6019a594f9e54faafc3f0a84c
Daniel Hall <daniel@danielhall.me>
2013-02-18 08:57:13 +1100

    Use generic class view for editing

--- staff/urls.py
+++ staff/urls.py
@@ -1,11 +1,17 @@
 from django.conf.urls import patterns, include, url
+from django.contrib.admin.views.decorators import staff_member_required
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.contrib.auth.models import User
 
 urlpatterns = patterns('staff.views',
+    # Views in views.py
     url(r'^$', 'home'),
     url(r'^userdetail/(?P<uid>\d+)/$','userdetail'),
 ) + patterns('django.views.generic.create_update',
-    url(r'^useredit/(?P<object_id>\d+)/$', 'update_object', {'model': User, 'template_name': 'staff_useredit.html', 'post_save_redirect':"/staff/"}),
+    # Generic Views
     url(r'^useradd/$', 'create_object', {'model': User, 'template_name': 'staff_useredit.html', 'post_save_redirect':"/staff/"}),
     url(r'^userdelete/(?P<object_id>\d+)/$', 'delete_object', {'model': User, 'template_name': 'staff_userdel.html', 'post_delete_redirect': '/staff/'}),
+) + patterns('',
+    # Class based views
+    url(r'^useredit/(?P<pk>\d+)/$', UpdateView.as_view(model=User, template_name='staff_useredit.html', success_url="/staff/")),
 )
