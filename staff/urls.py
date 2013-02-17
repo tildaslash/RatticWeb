ea8f963b9073b39f8252b40356118549f744264e
Daniel Hall <daniel.hall@rea-group.com>
2013-02-18 10:58:45 +1100

    Fixed the User delete stuff

--- staff/urls.py
+++ staff/urls.py
@@ -10,8 +10,8 @@ urlpatterns = patterns('staff.views',
 ) + patterns('django.views.generic.create_update',
     # Generic Views
     url(r'^useradd/$', 'create_object', {'model': User, 'template_name': 'staff_useredit.html', 'post_save_redirect':"/staff/"}),
-    url(r'^userdelete/(?P<object_id>\d+)/$', 'delete_object', {'model': User, 'template_name': 'staff_userdel.html', 'post_delete_redirect': '/staff/'}),
 ) + patterns('',
     # Class based views
     url(r'^useredit/(?P<pk>\d+)/$', staff_member_required(UpdateView.as_view(model=User, template_name='staff_useredit.html', success_url="/staff/"))),
+    url(r'^userdelete/(?P<pk>\d+)/$', staff_member_required(DeleteView.as_view(model=User, template_name='staff_userdel.html', success_url='/staff/'))),
 )
