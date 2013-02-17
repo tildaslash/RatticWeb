cbcf53aeecf219228619932eeeef0451edb7bf75
KP <kp@zemmiph0bia.com>
2013-02-17 17:06:07 +1100

    Can now access stored passwords

--- cred/urls.py
+++ cred/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('cred.views',
-    url(r'^list/$', 'list')
+    url(r'^list/$', 'list'),
+    url(r'^detail/(?P<cred_id>\d+)/$', 'detail'),
 )
-
--- cred/views.py
+++ cred/views.py
@@ -1,7 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from models import Cred
+from django.http import Http404
 
 def list(request):
     cred = Cred.objects.filter(group__in=request.user.groups.all())
     return render(request, 'cred_list.html', {'credlist': cred})
 
+def detail(request, cred_id):
+    cred = get_object_or_404(Cred, pk=cred_id)
+    if cred.group not in request.user.groups.all():
+        raise Http404
+    return render(request, 'cred_detail.html', {'cred' : cred})
