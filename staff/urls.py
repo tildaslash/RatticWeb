3a46ea8576fd62acfd14e0f049a7313db005631d
Daniel Hall <daniel.hall@rea-group.com>
2013-02-18 11:05:43 +1100

    Missed close parenthesis

--- staff/urls.py
+++ staff/urls.py
@@ -11,5 +11,5 @@ urlpatterns = patterns('staff.views',
     # Class based views
     url(r'^useredit/(?P<pk>\d+)/$',   staff_member_required(UpdateView.as_view(model=User, template_name='staff_useredit.html', success_url='/staff/'))),
     url(r'^userdelete/(?P<pk>\d+)/$', staff_member_required(DeleteView.as_view(model=User, template_name='staff_userdel.html',  success_url='/staff/'))),
-    url(r'^useradd/$',                staff_member_required(CreateView.as_view(model=User, template_name='staff_useredit.html', success_url='/staff/'}),
+    url(r'^useradd/$',                staff_member_required(CreateView.as_view(model=User, template_name='staff_useredit.html', success_url='/staff/'})),
 )
