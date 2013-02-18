bf8b67fe001bd48d8d4f67db67ca708ba0bf6f08
Daniel Hall <daniel.hall@rea-group.com>
2013-02-18 11:01:21 +1100

    Add user is now a class view and protected

--- staff/urls.py
+++ staff/urls.py
@@ -7,11 +7,9 @@ urlpatterns = patterns('staff.views',
     # Views in views.py
     url(r'^$', 'home'),
     url(r'^userdetail/(?P<uid>\d+)/$','userdetail'),
-) + patterns('django.views.generic.create_update',
-    # Generic Views
-    url(r'^useradd/$', 'create_object', {'model': User, 'template_name': 'staff_useredit.html', 'post_save_redirect':"/staff/"}),
 ) + patterns('',
     # Class based views
-    url(r'^useredit/(?P<pk>\d+)/$', staff_member_required(UpdateView.as_view(model=User, template_name='staff_useredit.html', success_url="/staff/"))),
-    url(r'^userdelete/(?P<pk>\d+)/$', staff_member_required(DeleteView.as_view(model=User, template_name='staff_userdel.html', success_url='/staff/'))),
+    url(r'^useredit/(?P<pk>\d+)/$',   staff_member_required(UpdateView.as_view(model=User, template_name='staff_useredit.html', success_url='/staff/'))),
+    url(r'^userdelete/(?P<pk>\d+)/$', staff_member_required(DeleteView.as_view(model=User, template_name='staff_userdel.html',  success_url='/staff/'))),
+    url(r'^useradd/$',                staff_member_required(CreateView.as_view(model=User, template_name='staff_useredit.html', success_url='/staff/'}),
 )
