2a72062e7376b8c933f41c142215b9dea8145f64
Daniel Hall <daniel@danielhall.me>
2013-02-25 09:02:33 +1100

    Users can now change their password Fixes #56

--- account/urls.py
+++ account/urls.py
@@ -8,5 +8,6 @@ urlpatterns = patterns('',
     url(r'^reset/done/$','django.contrib.auth.views.password_reset_done', {'template_name':'password_reset_done.html'}),
     url(r'^reset/(?P<uidb36>[0-9A-Za-z]+)-(?P<token>.+)/$', 'django.contrib.auth.views.password_reset_confirm', {'post_reset_redirect':'/', 'template_name':'password_reset_confirm.html'}),
     url(r'^profile/$', profile, {}),
+    url(r'^changepass/$', 'django.contrib.auth.views.password_change', {'post_change_redirect': '/account/profile/', 'template_name': 'account_changepass.html'})
 )
 
