fd671d28221708b9f14d4e373d9186072917e0d2
Daniel Hall <daniel.hall@rea-group.com>
2013-02-25 11:49:23 +1100

    Navbar highlighting, not the best method, but it works Fixes #37

--- ratticweb/context_processors.py
+++ ratticweb/context_processors.py
@@ -1,7 +1,10 @@
 from cred.models import Tag, CredChangeQ
 
 def base_template_reqs(request):
-    cntx = {'alltags': Tag.objects.all(),}
+    cntx = {
+        'alltags': Tag.objects.all(),
+        'pageurl': request.path,
+    }
 
     if request.user.is_authenticated():
         cntx['changeqcount'] = CredChangeQ.objects.for_user(request.user).count()
