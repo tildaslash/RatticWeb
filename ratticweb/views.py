a5e2795093160d1c17c017e780ad120541bf975f
Daniel Hall <daniel@danielhall.me>
2013-03-02 18:10:48 +1100

    Just fixing the homepage redirects

--- ratticweb/views.py
+++ ratticweb/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import redirect
+from django.core.urlresolvers import reverse
 
 def home(request):
     if request.user.is_authenticated():
-        return redirect('/cred/list')
+        return redirect(reverse('cred.views.list'))
     else:
-        return redirect('/account/login')
+        return redirect(reverse('django.contrib.auth.views.login'))
 
