7cb72fde1b563cd08b8bc291727c1add701e2d8a
Daniel Hall <daniel@danielhall.me>
2013-03-05 20:48:22 +1100

    Added management command to create users for the demo Fixes #92

---
+++ account/management/commands/demosetup.py
@@ -0,0 +1,16 @@
+from django.core.management.base import BaseCommand, CommandError
+from django.core.exceptions import ObjectDoesNotExist
+from django.contrib.auth.models import User
+
+class Command(BaseCommand):
+    help = 'Ensures there exists a user named "admin" with the password "rattic"'
+
+    def handle(self, *args, **options):
+        try:
+            u = User.objects.get(username='admin')
+        except ObjectDoesNotExist:
+            u = User(username='admin', email='admin@example.com')
+
+        u.set_password('rattic')
+        u.is_staff = True
+        u.save()
