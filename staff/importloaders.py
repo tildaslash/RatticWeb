89194b2c51d64f2df75c21c69f42e3af6d698c70
Daniel Hall <daniel.hall@rea-group.com>
2013-03-07 11:12:42 +1100

    Can now parse and process keepass files

--- staff/importloaders.py
+++ staff/importloaders.py
@@ -4,11 +4,29 @@ from lib.keepass import kpdb
 def keepass(filep, password):
     groups = []
     entries = []
+    groupstack = []
 
     try:
         db = kpdb.Database(filep, password)
     except AttributeError:
         return None
 
-    return {'groups': groups, 'entries': entries}
+    _walkkeepass(groups, entries, groupstack, db.hierarchy())
+
+    return {'tags': groups, 'entries': entries}
+
+def _walkkeepass(groups, entries, groupstack, root):
+    for n in root.nodes:
+      groupstack.append(n.name())
+      groups.append(n.name())
+      for e in n.entries:
+          entries.append({
+              'title': e.title,
+              'username': e.username,
+              'password': e.password,
+              'description': e.notes,
+              'tags': list(groupstack)
+          })
+      _walkkeepass(groups, entries, groupstack, n)
+      groupstack.pop()
 
