42372e62ad3a04f35ab9e189d47c92a499926d5d
Daniel Hall <daniel@danielhall.me>
2013-03-07 22:18:20 +1100

    Fix logout bug with context processor

--- ratticweb/context_processors.py
+++ ratticweb/context_processors.py
@@ -3,12 +3,14 @@ from django.db.models import Count
 
 def base_template_reqs(request):
     cntx = {
-        'alltags': Tag.objects.annotate(num_creds=Count('child_creds')).order_by('-num_creds')[:request.user.profile.tags_on_sidebar],
         'pageurl': request.path,
     }
 
     if request.user.is_authenticated():
         cntx['changeqcount'] = CredChangeQ.objects.for_user(request.user).count()
+        cntx['alltags'] = Tag.objects.annotate(num_creds=Count('child_creds')).order_by('-num_creds')[:request.user.profile.tags_on_sidebar]
+    else:
+        cntx['alltags'] = []
 
     return cntx
 
