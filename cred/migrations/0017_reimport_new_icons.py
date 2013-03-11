1fb678e773c0134bf8c7fd088ad487c7d9e4a3f4
Daniel Hall <daniel.hall@rea-group.com>
2013-03-12 09:35:55 +1100

    Broken migrations that only worked if you went at the time they were made

--- cred/migrations/0017_reimport_new_icons.py
+++ cred/migrations/0017_reimport_new_icons.py
@@ -4,13 +4,20 @@ from south.db import db
 from south.v2 import DataMigration
 from django.db import models
 
+from dingus import patch
+
+
+def loaddata(orm, fixture_name):
+    _get_model = lambda model_identifier: orm[model_identifier]
+
+    with patch('django.core.serializers.python._get_model', _get_model):
+        from django.core.management import call_command
+        call_command("loaddata", fixture_name)
+
 class Migration(DataMigration):
 
     def forwards(self, orm):
-        "Write your forwards methods here."
-        # Note: Remember to use orm['appname.ModelName'] rather than "from appname.models..."
-        from django.core.management import call_command
-        call_command("loaddata", "cred/db/initialicons_01.json")
+        loaddata(orm, "cred/db/initialicons_01.json")
 
     def backwards(self, orm):
         "Write your backwards methods here."
