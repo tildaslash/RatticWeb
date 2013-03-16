33176be40536021b2ed5bb0225e1e78eda4aa6cc
Daniel Hall <daniel@danielhall.me>
2013-03-16 16:52:23 +1100

    Test the deletion of objects[D

--- cred/tests.py
+++ cred/tests.py
@@ -9,7 +9,7 @@ from django.test import TestCase
 from models import Cred
 from django.contrib.auth.models import User, Group
 
-class SimpleTest(TestCase):
+class CredAccessTest(TestCase):
     def setUp(self):
         g = Group(name='h')
         g.save()
@@ -56,4 +56,31 @@ class SimpleTest(TestCase):
         self.assertTrue(self.d in Cred.objects.accessable(self.s, deleted=True))
         self.assertTrue(not self.d in Cred.objects.accessable(self.u, deleted=True))
 
+class CredDeleteTest(TestCase):
+    def setUp(self):
+        g = Group(name='h')
+        g.save()
+
+        c = Cred(title='testcred', password='1234', group=g)
+        c.save()
+
+        self.c = c
+
+    def test_delete(self):
+        c = self.c
+
+        c.delete()
+
+        self.assertEqual(Cred.objects.get(id=c.id), c)
+
+        test = False
+
+        c.delete()
+
+        try:
+            Cred.objects.get(id=c.id)
+        except Cred.DoesNotExist:
+            test = True
+
+        self.assertTrue(test)
 
