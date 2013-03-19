5c86fac5c9b93d37be1519bbf48c8f3817e28c6f
KP <kp@zemmiph0bia.com>
2013-03-19 20:32:12 +1100

    Password expiration on users now updates

--- account/models.py
+++ account/models.py
@@ -2,11 +2,15 @@ from django.db import models
 from django.forms import ModelForm
 from django.contrib import admin
 from django.contrib.auth.models import User
+from django.db.models.signals import pre_save
+from django.dispatch import receiver
+from datetime import datetime
 
 class UserProfile(models.Model):
   user = models.ForeignKey(User, unique=True)
   items_per_page = models.IntegerField(default=25)
   tags_on_sidebar = models.IntegerField(default=5)
+  password_changed = models.DateTimeField(default=datetime.now)
 
   def __unicode__(self):
     return self.user.username
@@ -14,10 +18,21 @@ class UserProfile(models.Model):
 class UserProfileForm(ModelForm):
     class Meta:
         model = UserProfile
-        exclude = ('user',)
+        exclude = ('user', 'password_changed',)
 
 # Attach the UserProfile object to the User
 User.profile = property(lambda u: UserProfile.objects.get_or_create(user=u)[0])
+
+@receiver(pre_save, sender=User)
+def user_save_handler(sender, instance, **kwargs):
+    try:
+        olduser = User.objects.get(id=instance.id)
+    except User.DoesNotExist:
+        return
+    if olduser.password != instance.password:
+        p = instance.profile
+        p.password_changed = datetime.now()
+        p.save()
 
 admin.site.register(UserProfile)
 
