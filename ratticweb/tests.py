bd49988b7f7fb86342bdb321e99721401ccc8956
Daniel Hall <daniel.hall@rea-group.com>
2013-03-27 15:53:09 +1100

    Make sure the admin site is never available

--- ratticweb/tests.py
+++ ratticweb/tests.py
@@ -10,3 +10,8 @@ class HomepageTest(TestCase):
         self.assertEqual(response.redirect_chain[0][1], 302)
         self.assertEqual(response.status_code, 200)
 
+    def test_admin_disabled(self):
+        client = Client()
+        response = client.get('/admin/')
+        self.assertEqual(response.status_code, 404)
+
