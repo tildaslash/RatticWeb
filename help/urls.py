24dae9b5b320fbac771f04a2f0215fe7d3b415be
Daniel Hall <daniel@danielhall.me>
2013-03-29 22:17:12 +1100

    Refactoring the help views, show link

--- help/urls.py
+++ help/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('help.views',
-    url(r'^$', 'help_home'),
-    url(r'^(?P<page>[\w]+)/$', 'help_markdown'),
+    url(r'^$', 'home'),
+    url(r'^(?P<page>[\w]+)/$', 'markdown'),
 )
--- help/views.py
+++ help/views.py
@@ -5,13 +5,13 @@ from django.core.urlresolvers import reverse
 
 import os
 
-def help_markdown(request, page):
+def markdown(request, page):
     if settings.HELP_SYSTEM_FILES == False:
         return render(request, 'help_nohelp.html', {})
 
     filename = os.path.join(settings.HELP_SYSTEM_FILES, page + '.md')
     return render(request, 'help_markdown.html', {'file': filename})
 
-def help_home(request):
-    return help_markdown(request, 'Home')
+def home(request):
+    return markdown(request, 'Home')
 
