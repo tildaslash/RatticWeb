99b64c038d1ae4f0ee80abed43dfdb463d8375f2
Daniel Hall <daniel@danielhall.me>
2013-04-18 19:29:58 +1000

    Allow hyphens is help urls Fixes #148

--- help/urls.py
+++ help/urls.py
@@ -2,5 +2,5 @@ from django.conf.urls import patterns, url
 
 urlpatterns = patterns('help.views',
     url(r'^$', 'home'),
-    url(r'^(?P<page>[\w]+)/$', 'markdown'),
+    url(r'^(?P<page>[\w\-]+)/$', 'markdown'),
 )
