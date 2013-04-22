fa1cbb95b44c10ced6cb53a33281426d80d7614a
Daniel Hall <daniel@danielhall.me>
2013-04-22 20:51:49 +1000

    Let the API Key be created the first time its viewed. Fixes #146

--- cred/models.py
+++ cred/models.py
@@ -6,9 +6,6 @@ from django.forms import ModelForm, SelectMultiple
 
 from tastypie.models import create_api_key
 
-# Every time a user is saved, make sure they have an API key.
-models.signals.post_save.connect(create_api_key, sender=User)
-
 
 class Tag(models.Model):
     name = models.CharField(max_length=64)
