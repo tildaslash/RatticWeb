4ea289eeaede1f1687c049e5120d95c721366da9
Daniel Hall <daniel.hall@rea-group.com>
2013-04-26 16:10:33 +1000

    Pull the credentials view back into one file

--- cred/shortcut.py
+++ cred/shortcut.py
@@ -3,6 +3,7 @@ from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 
 def render_credlist(request, credlist, title='All passwords'):
+    # Paginate the credlist
     paginator = Paginator(credlist, request.user.profile.items_per_page)
     page = request.GET.get('page')
     try:
@@ -11,7 +12,9 @@ def render_credlist(request, credlist, title='All passwords'):
         cred = paginator.page(1)
     except EmptyPage:
         cred = paginator.page(paginator.num_pages)
+
+    # Show the template
     return render(request, 'cred_list.html', {
         'credlist': cred,
-        'credtitle': title
+        'credtitle': title,
     })
