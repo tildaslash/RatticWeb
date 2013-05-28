92a322a5621cfeca419be36db731e5e1b9a8d0a3
Daniel Hall <daniel.hall@rea-group.com>
2013-05-28 13:42:36 +1000

    Make LDAP start on Ubuntu, Debian while still working on RHEL, Fedora

--- docs/ldap/startldap.sh
+++ docs/ldap/startldap.sh
@@ -3,6 +3,13 @@
 export LDAPNOINIT=yes
 
 LDAPBASE=$(mktemp --tmpdir=/tmp -d ratticldap.XXXXX)
+
+if [ -f /etc/openldap/schema/core.schema ]; then
+  SCHEMABASE=/etc/openldap/schema
+else
+  SCHEMABASE=/etc/ldap/schema
+fi
+
 echo ${LDAPBASE} > .ldapbase
 
 mkdir ${LDAPBASE}/db
@@ -11,6 +18,7 @@ cp DB_CONFIG ${LDAPBASE}/db
 
 sed -i "s|XXX_LDAP_PID_XXX|${LDAPBASE}/slapd.pid|" ${LDAPBASE}/slapd.conf
 sed -i "s|XXX_LDAP_DATA_DIR_XXX|${LDAPBASE}/db|" ${LDAPBASE}/slapd.conf
+sed -i "s|XXX_SCHEMA_LOCATION_XXX|${SCHEMABASE}|" ${LDAPBASE}/slapd.conf
 
 nohup slapd -d3 -f ${LDAPBASE}/slapd.conf -h 'ldap://localhost:3389/' &> ${LDAPBASE}/slapd.log &
 
