53775a0365ca5708d7f2ba37c8a4f16ee27a2c74
Daniel Hall <daniel.hall@rea-group.com>
2013-05-28 13:29:50 +1000

    Much more LDAP logging

--- docs/ldap/startldap.sh
+++ docs/ldap/startldap.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+export LDAPNOINIT=yes
+
 LDAPBASE=$(mktemp --tmpdir=/tmp -d ratticldap.XXXXX)
 echo ${LDAPBASE} > .ldapbase
 
@@ -10,10 +12,12 @@ cp DB_CONFIG ${LDAPBASE}/db
 sed -i "s|XXX_LDAP_PID_XXX|${LDAPBASE}/slapd.pid|" ${LDAPBASE}/slapd.conf
 sed -i "s|XXX_LDAP_DATA_DIR_XXX|${LDAPBASE}/db|" ${LDAPBASE}/slapd.conf
 
-LDAPNOINIT=yes slapd -f ${LDAPBASE}/slapd.conf -h 'ldap://localhost:3389/'
+nohup slapd -d3 -f ${LDAPBASE}/slapd.conf -h 'ldap://localhost:3389/' &> ${LDAPBASE}/slapd.log &
 
 # Give LDAP a few seconds to start
 sleep 3
+
+cat ${LDAPBASE}/slapd.log
 
 ldapadd -x -D 'cn=manager,dc=example,dc=com' -w testpass -H 'ldap://localhost:3389/' -f initial.ldif > /dev/null
 echo "LDAP Started"
