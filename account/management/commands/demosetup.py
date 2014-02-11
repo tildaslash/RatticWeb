0f0b7e8bc0f92dce29faffeb0ab0ac97fc123e83
Daniel Hall <daniel@danielhall.me>
2014-02-11 21:26:43 +1100

    Update the demo user setup for Two factor authentication

--- account/management/commands/demosetup.py
+++ account/management/commands/demosetup.py
@@ -7,11 +7,21 @@ class Command(BaseCommand):
     help = 'Ensures there exists a user named "admin" with the password "rattic"'
 
     def handle(self, *args, **options):
+        # Create an admin user or find one if it exists
         try:
             u = User.objects.get(username='admin')
         except ObjectDoesNotExist:
             u = User(username='admin', email='admin@example.com')
 
+        # (Re)set the password to rattic
         u.set_password('rattic')
+
+        # Make them a staff member
         u.is_staff = True
+
+        # Save the user
         u.save()
+
+        # Delete any tokens they may have
+        for dev in devices_for_user(u):
+            dev.delete()
