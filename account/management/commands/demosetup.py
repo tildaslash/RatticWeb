de789e11b692afbd7f7c67e4be89ca8bd415152f
Daniel Hall <daniel.hall@rea-group.com>
2014-02-12 11:17:46 +1100

    Import devices_for_user for the demosetup command

--- account/management/commands/demosetup.py
+++ account/management/commands/demosetup.py
@@ -1,6 +1,7 @@
 from django.core.management.base import BaseCommand
 from django.core.exceptions import ObjectDoesNotExist
 from django.contrib.auth.models import User
+from django_otp import devices_for_user
 
 
 class Command(BaseCommand):
