2301d34fc1837d0e96a7ae5b9e3f20825e273548
Daniel Hall <daniel@danielhall.me>
2014-05-12 18:06:29 +1000

    Better documentation for the credicons templatetag

--- cred/templatetags/credicons.py
+++ cred/templatetags/credicons.py
@@ -5,28 +5,32 @@ from cred.icon import get_icon_data
 
 register = template.Library()
 
-# Sprite Tag
-# <img alt="Guitar" style="height: 10px; width: 10px; background:
-# url(/static/rattic/img/sprite.png) -1078px -0px;"
-# src="/static/rattic/img/clear.gif">
-
-# Non-sprite Tag
-# <img src="/static/rattic/img/credicon/Key.png">
-
 
 @register.simple_tag
 def cred_icon(iconname, txtfield=None, imgfield=None, tagid=None):
+    """
+    Generates the HTML used to display an icon. If the icon cannot be found
+    it reverts to the default icon mentioned in settings CRED_ICON_DEFAULT.
+    It is used on the details pages and as part of the CredIconChooser widget.
+    """
+
+    # Get info for the icon, if that wont work, get info for the default icon
     try:
         data = get_icon_data()[iconname]
     except KeyError:
         data = get_icon_data()[settings.CRED_ICON_DEFAULT]
 
+    # Get the URL for the blank image (goes in the href)
     blankimg = settings.STATIC_URL + settings.CRED_ICON_CLEAR
+
+    # What CSS classes does our icon need
     classes = ['rattic-icon', data['css-class']]
 
+    # If the icon is clickable append the clickable class
     if txtfield is not None or imgfield is not None:
         classes.append('rattic-icon-clickable')
 
+    # Build the HTML
     tag = '<img '
     tag += 'src="%s" ' % blankimg
     tag += 'data-icon-name="%s" ' % data['filename']
