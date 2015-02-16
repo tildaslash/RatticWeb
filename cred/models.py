48c2e247db041c006d41d1f705d09dfc6447d38c
Mike Ray <mike.joseph.ray@gmail.com>
2015-02-16 15:43:28 -0600

    removing URL validation

--- cred/models.py
+++ cred/models.py
@@ -75,7 +75,7 @@ class Cred(models.Model):
 
     # User changable fields
     title = models.CharField(max_length=64, db_index=True)
-    url = models.CharField(blank=True, null=True, db_index=True, validators=[URIValidator()], max_length=1024)
+    url = models.URLField(blank=True, null=True, db_index=True)
     username = models.CharField(max_length=250, blank=True, null=True, db_index=True)
     password = models.CharField(max_length=250, blank=True, null=True)
     descriptionmarkdown = models.BooleanField(default=False, verbose_name=_('Markdown Description'))
